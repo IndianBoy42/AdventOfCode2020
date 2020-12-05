b998fc34167a2418b6245d53e9f10b5026cad749
IndianBoy42 <rohanmed123@gmail.com>
2020-12-05 09:32:53 +0800

    ready for day 5

---
+++ src/day5.rs
@@ -0,0 +1,15 @@
+use crate::utils::*;
+
+pub fn part1(input: &str) -> usize {
+    unimplemented!();
+}
+pub fn part2(input: &str) -> usize {
+    unimplemented!();
+}
+
+#[test]
+fn test5() {
+    let input = read_input("input5.txt").unwrap();
+    assert_eq!(part1(&input), 0);
+    assert_eq!(part2(&input), 0);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use aoc20::day1;
 use aoc20::day2;
 use aoc20::day3;
 use aoc20::day4;
+use aoc20::day5;
 use aoc20::utils;
 
 fn main() {
@@ -22,4 +23,8 @@ fn main() {
     let input = utils::read_input("input4.txt").unwrap();
     dbg!(day4::part1(&input));
     dbg!(day4::part2(&input));
+
+    let input = utils::read_input("input5.txt").unwrap();
+    dbg!(day5::part1(&input));
+    dbg!(day5::part2(&input));
 }
