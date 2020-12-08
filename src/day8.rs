ebbd9cb5727d47126a4478db697593c12dcd3c09
IndianBoy42 <rohanmed123@gmail.com>
2020-12-08 17:21:12 +0800

    multithreaded day8, but still not fast enough, synchronization overhead is too big?

--- src/day8.rs
+++ src/day8.rs
@@ -1,3 +1,6 @@
+use core::sync::atomic::{self, AtomicBool};
+use std::time::Instant;
+
 use crate::utils::*;
 
 type Ins<'a> = &'a str;
@@ -11,6 +14,34 @@ fn parse(input: &str) -> Vec<(Ins, i32)> {
         .map(|(ins, arg)| (ins, arg.parse().unwrap()))
         .collect()
 }
+
+// This is slower for some reason
+// #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
+// enum Ins {
+//     ACC,
+//     JMP,
+//     NOP,
+// }
+// const INS_ACC: Ins = Ins::ACC;
+// const INS_JMP: Ins = Ins::JMP;
+// const INS_NOP: Ins = Ins::NOP;
+// fn parse(input: &str) -> Vec<(Ins, i32)> {
+//     input
+//         .lines()
+//         .map(|line| line.split_once(' ').unwrap())
+//         .map(|(ins, arg)| {
+//             (
+//                 match ins {
+//                     "acc" => INS_ACC,
+//                     "jmp" => INS_JMP,
+//                     "nop" => INS_NOP,
+//                     _ => unreachable!(),
+//                 },
+//                 arg.parse().unwrap(),
+//             )
+//         })
+//         .collect()
+// }
 
 fn solve(program: &[(Ins, i32)]) -> (i32, i32) {
     let mut acc = 0;
@@ -34,11 +65,6 @@ fn solve(program: &[(Ins, i32)]) -> (i32, i32) {
 
     (pc, acc)
 }
-enum OpCode {
-    NOP,
-    JMP,
-    ACC,
-}
 
 pub fn part1(input: &str) -> i32 {
     let program = parse(input);
@@ -46,42 +72,98 @@ pub fn part1(input: &str) -> i32 {
     solve(&program).1
 }
 
+use crossbeam::channel;
+
 pub fn part2(input: &str) -> i32 {
     let mut program = parse(input);
 
     if false {
-        program
-            .clone()
-            .par_iter()
-            .enumerate()
-            .filter(|(_,&(ins, _))| ins != INS_ACC)
-            .find_map_any(|(i, &(ins, _))| {
-                let mut program = program.clone();
+        // let start = Instant::now();
+        let finished = AtomicBool::default();
 
-                // let old = ins;
-                program[i].0 = match ins {
-                    INS_NOP => INS_JMP,
-                    INS_JMP => INS_NOP,
-                    _ => unreachable!(),
-                };
+        let res = crossbeam::scope(|scope| {
+            const NTHREADS: usize = 4;
 
-                let (pc, acc) = solve(&program);
-                // program[i].0 = old;
+            let program = &program;
+            let threads = (0..NTHREADS)
+                .map(|thread_idx| {
+                    // For sending the result back
+                    let (s, r) = channel::bounded(1);
+                    let s2 = s.clone();
+                    let finished = &finished;
 
-                if pc as usize == program.len() {
-                    Some(acc)
-                } else {
-                    None
-                }
-            }).unwrap()
+                    let th = scope.spawn(move |_| {
+                        let s = s;
+                        let mut exe = program.clone();
+                        let iter = program
+                            .iter()
+                            .enumerate()
+                            .skip(thread_idx)
+                            .step_by(NTHREADS);
+                        for (i, &(ins, _)) in iter {
+                            // Check for cancellation
+                            if i % 16 == 0 && finished.load(atomic::Ordering::Acquire) {
+                                return;
+                            }
+                            // Process as normal
+                            if ins == INS_ACC {
+                                continue;
+                            }
+
+                            exe[i].0 = match ins {
+                                INS_NOP => INS_JMP,
+                                INS_JMP => INS_NOP,
+                                _ => unreachable!(),
+                            };
+
+                            let (pc, acc) = solve(&exe);
+                            if pc as usize == exe.len() {
+                                (s.send(acc)).unwrap();
+                                return;
+                            }
+
+                            exe[i].0 = ins;
+                        }
+                        //not found, hopefully another thread finds
+                    });
+                    (th, r, s2)
+                })
+                .collect_vec();
+
+            // Select over the result recv channels of all threads
+            let mut sel = channel::Select::new();
+            for (_, r, _) in &threads {
+                sel.recv(r);
+            }
+            let sel = (sel.select());
+            // This thread found it
+            let selindex = sel.index();
+            // Complete the recv operation
+            let res = (sel.recv(&threads[selindex].1));
+            // Stop all the other threads
+            // threads
+            //     .iter()
+            //     .enumerate()
+            //     .filter(|&(i, _)| i != selindex)
+            //     .for_each(|(_, (_, _, cs, _, _))| {
+            //         (cs.send(true));
+            //     });
+            finished.store(true, atomic::Ordering::Release);
+
+            return res.unwrap();
+        });
+
+        // let dur = Instant::now() - start;
+        // println!("{:?}", dur);
+
+        res.unwrap()
     } else {
-        for (i, (ins, _)) in program.clone().iter_mut().enumerate() {
-            if *ins == INS_ACC {
+        for (i, &(ins, _)) in program.clone().iter().enumerate() {
+            if ins == INS_ACC {
                 continue;
             }
 
-            let old = *ins;
-            program[i].0 = match old {
+            program[i].0 = match ins {
                 INS_NOP => INS_JMP,
                 INS_JMP => INS_NOP,
                 _ => unreachable!(),
@@ -92,7 +174,7 @@ pub fn part2(input: &str) -> i32 {
                 return acc;
             }
 
-            program[i].0 = old;
+            program[i].0 = ins;
         }
         unreachable!()
     }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use aoc20::{day8, utils::read_input};
+
 
 fn main() {
-    println!("Hello, world!");
+    let input = read_input("input8.txt").unwrap();
+    
+    for _ in 0..10 {
+        day8::part2(&input);
+    }
 }
