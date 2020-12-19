d9d790f2e95d69670b08dbf08221c138d27e844b
IndianBoy42 <rohanmed123@gmail.com>
2020-12-19 21:00:53 +0800

    day18 and day19

--- src/day18.rs
+++ src/day18.rs
@@ -1,18 +1,181 @@
+use std::iter::Peekable;
+
+use itertools::{unfold, PeekingNext};
+use regex::CaptureMatches;
+use regex::Regex;
+
 use crate::utils::*;
 
-pub fn part1(input: &str) -> usize {
-    input.lines().map(|line| line);
-    unimplemented!()
+fn num(input: &str) -> i64 {
+    input.trim().parse().unwrap()
 }
 
-pub fn part2(input: &str) -> usize {
-    input.lines().map(|line| line);
-    unimplemented!()
+macro_rules! dbg {
+    ($t:expr) => {
+        $t
+    };
+}
+
+fn evalone<'a>(tokens: &mut impl Iterator<Item = &'a str>) -> i64 {
+    match dbg!(tokens.next().unwrap()) {
+        "+" => unreachable!(),
+        "*" => unreachable!(),
+        "(" => {
+            // tokens.next();
+            eval(tokens)
+        }
+        ")" => unreachable!(),
+        token => {
+            // tokens.next();
+            num(token)
+        }
+    }
+}
+
+fn eval<'a>(tokens: &mut impl Iterator<Item = &'a str>) -> i64 {
+    let mut acc = 0;
+    while let Some(token) = tokens.next() {
+        match dbg!(token) {
+            "+" => {
+                acc += dbg!(evalone(tokens));
+                dbg!(acc);
+            }
+            "*" => {
+                acc *= dbg!(evalone(tokens));
+                dbg!(acc);
+            }
+            "(" => {
+                acc = eval(tokens);
+                dbg!(acc);
+            }
+            ")" => break,
+            token => {
+                let num = num(token);
+                acc = num;
+                dbg!(acc);
+            }
+        }
+    }
+
+    dbg!(acc)
+}
+
+fn tokens(input: &str) -> impl Iterator<Item = &str> {
+    lazy_static::lazy_static! {
+        static ref RE: Regex = Regex::new(r"([0-9]+|\*|\+|\(|\))").unwrap();
+    }
+    RE.captures_iter(input)
+        .map(|cap| cap.get(0).unwrap().as_str())
+}
+
+pub fn part1(input: &str) -> i64 {
+    input
+        .lines()
+        .map(|line| dbg!(tokens(line).collect_vec()))
+        // .take(1)
+        .map(|line| eval(&mut line.into_iter()))
+        .sum()
+}
+
+fn evalone2<'a>(tokens: &mut Peekable<impl Iterator<Item = &'a str>>) -> i64 {
+    match dbg!(tokens.next().unwrap()) {
+        "+" => unreachable!(),
+        "*" => unreachable!(),
+        "(" => {
+            // tokens.next();
+            eval2(tokens)
+        }
+        ")" => unreachable!(),
+        token => {
+            // tokens.next();
+            num(token)
+        }
+    }
+}
+fn eval_star<'a>(tokens: &mut Peekable<impl Iterator<Item = &'a str>>) -> i64 {
+    let mut acc = 0;
+    // let mut tokens = tokens.peeking_take_while(|&token| token != "*");
+    while let Some(&token) = tokens.peek() {
+        match dbg!(token) {
+            "+" => {
+                tokens.next();
+                acc += dbg!(evalone2(tokens));
+                dbg!(acc);
+            }
+            "*" => break,
+            "(" => {
+                tokens.next();
+                acc = eval2(tokens);
+                dbg!(acc);
+            }
+            ")" => break,
+            token => {
+                tokens.next();
+                let num = num(token);
+                acc = num;
+                dbg!(acc);
+            }
+        }
+    }
+
+    dbg!(acc)
+}
+fn eval2<'a>(tokens: &mut Peekable<impl Iterator<Item = &'a str>>) -> i64 {
+    let mut acc = 0;
+    while let Some(token) = tokens.next() {
+        match dbg!(token) {
+            "+" => {
+                acc += dbg!(evalone2(tokens));
+                dbg!(acc);
+            }
+            "*" => {
+                acc *= dbg!(eval_star(tokens));
+                dbg!(acc);
+            }
+            "(" => {
+                acc = eval2(tokens);
+                dbg!(acc);
+            }
+            ")" => break,
+            token => {
+                let num = num(token);
+                acc = num;
+                dbg!(acc);
+            }
+        }
+    }
+
+    dbg!(acc)
+}
+
+pub fn part2(input: &str) -> i64 {
+    input
+        .lines()
+        .map(|line| dbg!(tokens(line).collect_vec()))
+        // .take(1)
+        .map(|line| eval2(&mut line.into_iter().peekable()))
+        .sum()
 }
 
 #[test]
 fn test() {
     let input = read_input("input18.txt").unwrap();
-    assert_eq!(part1(&input), 0);
+    assert_eq!(part1("2 * 3 + (4 * 5)"), 26);
+    assert_eq!(part1("5 + (8 * 3 + 9 + 3 * 4 * 3)"), 437);
+    assert_eq!(part1("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"), 12240);
+    assert_eq!(
+        part1("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"),
+        13632
+    );
+    assert_eq!(part1(&input), 11004703763391);
+
+    assert_eq!(part2("1 + 2 * 3 + 4 * 5 + 6"), 231);
+    assert_eq!(part2("2 * 3 + (4 * 5)"), 46);
+    assert_eq!(part2("5 + (8 * 3 + 9 + 3 * 4 * 3)"), 1445);
+    assert_eq!(part2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"), 669060);
+    assert_eq!(
+        part2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"),
+        23340
+    );
     assert_eq!(part2(&input), 0);
 }
--- src/day19.rs
+++ src/day19.rs
@@ -1,18 +1,109 @@
+use std::borrow::Cow;
+
 use crate::utils::*;
+use regex::Regex;
+
+enum Rule<'a> {
+    Compound(Vec<Vec<i32>>),
+    Basic(&'a str),
+}
+impl Rule<'_> {
+    fn new(i: i32, specs: &str) -> Rule {
+        if let Some(rule) = specs.trim().strip_prefix('"') {
+            let rule = rule.strip_suffix('"').unwrap();
+            Rule::Basic(rule)
+        } else {
+            let specs = specs
+                .split('|')
+                .map(|rule| {
+                    rule.trim()
+                        .split(' ')
+                        .map(|n| n.trim().parse::<i32>().unwrap())
+                        .collect_vec()
+                })
+                .collect_vec();
+            Rule::Compound(specs)
+        }
+    }
+}
+
+type Dict<'a> = FMap<i32, Rule<'a>>;
+
+fn expand_regex<'a, 'b>(i: i32, dict: &'a Dict<'b>) -> Option<Cow<'b, str>> {
+    fn inner<'a, 'b>(i: i32, dict: &'a Dict<'b>, depth: i32) -> Option<Cow<'b, str>> {
+        if depth > 10 {
+            None?
+        }
+        let rule = dict.get(&i)?;
+        let rule: Cow<str> = match rule {
+            Rule::Compound(rules) => {
+                let rule = rules
+                    .iter()
+                    .map(|rule| {
+                        rule.iter()
+                            .filter_map(|&subrule| {
+                                inner(subrule, dict, if subrule == i { depth + 1 } else { 0 })
+                            })
+                            .join("")
+                    })
+                    .join("|");
+                Cow::Owned(format!("({})", rule))
+            }
+            Rule::Basic(rule) => Cow::Borrowed(rule),
+        };
+        Some(rule)
+    }
+    inner(i, dict, 0)
+}
 
 pub fn part1(input: &str) -> usize {
-    input.lines().map(|line| line);
-    unimplemented!()
+    let (rules, lines) = input.split_once("\n\n").unwrap();
+    let rules: Dict = rules
+        .lines()
+        .map(|line| line.split_once(':').unwrap())
+        .map(|(ruleno, specs)| {
+            let ruleno = ruleno.parse().unwrap();
+            (ruleno, Rule::new(ruleno, specs))
+        })
+        .collect();
+
+    let rule0 = dbg!(expand_regex(0, &rules)).unwrap();
+    let rule0 = format!("^{}$", rule0);
+    let rule0 = Regex::new(&rule0).unwrap();
+
+    lines.lines().filter(|line| rule0.is_match(line)).count()
 }
 
 pub fn part2(input: &str) -> usize {
-    input.lines().map(|line| line);
-    unimplemented!()
+    let (rules, lines) = input.split_once("\n\n").unwrap();
+    let rules: Dict = rules
+        .lines()
+        .map(|line| line.split_once(':').unwrap())
+        .map(|(ruleno, specs)| {
+            let ruleno = ruleno.parse().unwrap();
+            let specs = if ruleno == 8 {
+                " 42 | 42 8"
+            } else if ruleno == 11 {
+                " 42 31 | 42 11 31"
+            } else {
+                specs
+            };
+            (ruleno, Rule::new(ruleno, specs))
+        })
+        .collect();
+
+    let rule0 = dbg!(expand_regex(0, &rules)).unwrap();
+    let rule0 = format!("^{}$", rule0);
+    let rule0 = Regex::new(&rule0).unwrap();
+
+    lines.lines().filter(|line| rule0.is_match(line)).count()
 }
 
 #[test]
 fn test() {
+    // let input = read_input("test.txt").unwrap();
     let input = read_input("input19.txt").unwrap();
-    assert_eq!(part1(&input), 0);
+    assert_eq!(part1(&input), 124);
+    // dbg!(part1(&input));
     assert_eq!(part2(&input), 0);
 }
