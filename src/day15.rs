01b42d6dc7df857fdc652f33c7fb27d5ab0940cc
Anshuman <amedhi@connect.ust.hk>
2021-03-26 17:42:01 +0800

    split hashmaps (with nohash for the dense portion)
    approaching the 1 second barrier

--- src/day15.rs
+++ src/day15.rs
@@ -3,8 +3,28 @@ use std::{mem, unreachable};
 
 use crate::utils::*;
 
+// const SPLIT: usize = 1_000_000;
+const SPLIT: usize = 500_000;
+const DENSE_CAPACITY: usize = SPLIT;
+const SPARSE_CAPACITY: usize = SPLIT;
+
+use nohash_hasher::IntMap;
+
+macro_rules! entry {
+    ($map:tt, $densemap:tt, $u:tt) => {
+        // $densemap.entry($u)
+        if $u < SPLIT {
+            $densemap.entry($u)
+        } else {
+            $map.entry($u)
+        }
+    };
+}
+
 fn solve(input: &str, n: usize) -> usize {
-    let mut map: FMap<usize, (usize, usize)> = fmap(2020);
+    let mut map: FMap<usize, (usize, usize)> = fmap((n * 2 + 1).min(SPARSE_CAPACITY));
+    let mut densemap: IntMap<usize, (usize, usize)> =
+        IntMap::with_capacity_and_hasher((n * 2 + 1).min(DENSE_CAPACITY), Default::default());
 
     let starting_count = input.split(',').count();
 
@@ -15,7 +35,9 @@ fn solve(input: &str, n: usize) -> usize {
         .map(Result::unwrap)
         .enumerate()
         .for_each(|(i, num)| {
+            // entry!(map, densemap, num).or_insert((i, i));
             map.insert(num, (i, i));
+            densemap.insert(num, (i, i));
             last = num;
         });
 
@@ -25,7 +47,8 @@ fn solve(input: &str, n: usize) -> usize {
     //     .and_then(|s| s.parse().ok())
     //     .unwrap();
 
-    match map.entry(last) {
+    match entry!(map, densemap, last) {
+        // match map.entry(last) {
         Entry::Occupied(mut occ) => {
             let (prev, prev2) = occ.get_mut();
             last = *prev - *prev2;
@@ -35,8 +58,9 @@ fn solve(input: &str, n: usize) -> usize {
         }
     }
 
-    for i in starting_count..n {
-        match map.entry(last) {
+    for i in starting_count..(n - 1) {
+        match entry!(map, densemap, last) {
+            // match map.entry(last) {
             Entry::Occupied(mut occ) => {
                 let (prev, prev2) = occ.get_mut();
                 *prev2 = *prev;
